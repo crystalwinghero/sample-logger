bcbc115f31c7b3a4e457d4b9199c4545b0452f42
Crystalwing Bakaboe <powerful.crystal.wing@gmail.com>
2024-10-10 11:53:03 +0800

    Initial Commit

---
+++ sample-loggerTests/sample_loggerTests.swift
@@ -0,0 +1,17 @@
+//
+//  sample_loggerTests.swift
+//  sample-loggerTests
+//
+//  Created by Crystalwing Bakaboe on 10/10/24.
+//
+
+import Testing
+@testable import sample_logger
+
+struct sample_loggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
